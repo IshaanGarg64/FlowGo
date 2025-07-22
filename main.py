21ab9f5cc60988c9a7a769a108504992d192ecb8
Ishaan Garg <174088444+IshaanGarg64@users.noreply.github.com>
2025-07-22 17:05:05 +0530

    Feature: Added detection zone rectangle

--- main.py
+++ main.py
@@ -1,22 +1,18 @@
-# Import the OpenCV library, our main tool
 import cv2
 
-# Find your primary webcam (usually numbered 0)
 cap = cv2.VideoCapture(0)
 
-# Create a loop that runs forever to show a live video feed
 while True:
-    # Read one single image (a frame) from the webcam
     ret, frame = cap.read()
 
-    # If the frame was read correctly, display it
     if ret:
+        # This new line draws the green rectangle
+        cv2.rectangle(frame, (100, 80), (540, 400), (0, 255, 0), 2)
+
         cv2.imshow("FlowGo v0.1", frame)
 
-    # Create an exit condition: if the 'q' key is pressed, break the loop
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
-# Cleanly shut down the camera and close all windows
 cap.release()
 cv2.destroyAllWindows()
